6694a3000b962747a8be034c42aff6293d05748f
Emanuel Tello <etello@fi.uba.ar>
2023-05-14 14:35:19 -0300

    Cambio en ids de transacciones

--- src/main/java/com/aninfo/model/Transaction.java
+++ src/main/java/com/aninfo/model/Transaction.java
@@ -7,7 +7,7 @@ import java.util.Date;
 public class Transaction {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long transactionId;
 
     private Long accountCBU;
